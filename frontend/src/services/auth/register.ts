16365ff180805dba6fc87dffc6d89fdfe33b05e6
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-24 20:13:04 -0300

    fix: update register function to use environment variable for API server URL

--- frontend/src/services/auth/register.ts
+++ frontend/src/services/auth/register.ts
@@ -9,13 +9,16 @@ interface RegisterProps {
 }
 
 export async function register({ data }: RegisterProps) {
-  const res = await fetch("http://localhost:3000/auth/register", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const res = await fetch(
+    `${process.env.API_SERVER_BASE_URL}/api/auth/register`,
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
   if (res.ok) {
     signIn("credentials", {
       email: data.email,
