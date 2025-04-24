0a429a915529d6a27fae8d65ec949681cee62c80
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-23 22:18:35 -0300

    feat: implement refresh token functionality in AuthService

--- backend/src/modules/auth/services/auth-service.ts
+++ backend/src/modules/auth/services/auth-service.ts
@@ -1,4 +1,5 @@
 import { env } from "@/env";
+import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { IAuthRepository } from "../interfaces/auth";
@@ -55,13 +56,61 @@ export class AuthService {
       }
     );
 
+    const refreshToken = jwt.sign(
+      { id: user.id },
+      env.REFRESH_TOKEN_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
+
     return {
       user: {
         id: user.id,
         name: user.name,
         email: user.email,
       },
+      refreshToken,
       accessToken,
+    };
+  }
+
+  async refreshToken(token: string) {
+    if (!token) {
+      throw new Error("Token not provided");
+    }
+
+    const decoded = jwt.verify(token, env.REFRESH_TOKEN_SECRET as string) as {
+      id: string;
+    };
+
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const newAccessToken = jwt.sign(
+      { id: user.id },
+      env.AUTH_SECRET as string,
+      {
+        expiresIn: "15m",
+      }
+    );
+
+    const newRefreshToken = jwt.sign(
+      { id: user.id },
+      env.REFRESH_TOKEN_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    return {
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
     };
   }
 }
